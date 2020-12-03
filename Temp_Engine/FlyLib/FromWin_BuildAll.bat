78132f1130c129e9621e2a83be1157a3145e7546
Marc Torres Jimenez <mtorres.form@gmail.com>
2020-12-03 11:35:38 +0100

    Reordering Project and pulling and building only needed of GLFW
    Now GLFW can be fetched with the cmake building and will only build the dll required.
    The issue now is where it will be built on different platforms and if I will have to generate multiple folderpaths
    
    For now, the build will now be in ./builds/_dep/lib-build/src/CMAKE_BUILD_TYPE
    But that is only tested on windows (will have to check on Linux and adapt, maybe on MacOS some day).
    CMAKE_BUILD_TYPE is not cached for build on windows, the binaries are built directly for Debug, MinSizeRelease, Release and something else. You have to specify on --build which one to, makes it a bit hard to create proper build dynamically at first glance.

--- Temp_Engine/FlyLib/FromWin_BuildAll.bat
+++ Temp_Engine/FlyLib/FromWin_BuildAll.bat
@@ -2,5 +2,5 @@ mkdir cmake_bloat_towin &
 cd cmake_bloat_towin & 
 cmake ../ -UToAndroid -UToLinux -DToWindows=1 -DCMAKE_BUILD_TYPE=Release
 
-cmake --build .
+cmake --build . --config Release
 cmd /k
