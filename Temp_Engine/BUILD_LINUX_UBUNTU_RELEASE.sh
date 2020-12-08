f6910570b780f890a905f93576f09b005e1a6601
Marc Torres Jimenez <mtorres.form@gmail.com>
2020-12-08 12:31:50 +0100

    Back to undefined ANativeActivit_onCreate
    I will reformat again to get it easier to read because It still is too convoluted to know which parts are failing
    
    Seems like setting up in single project works teter than subdirectory, I don't know why though.
    
    Will take maybe a couple days to refactor cleanly and then will have to retest.

--- Temp_Engine/BUILD_LINUX_UBUNTU_RELEASE.sh
+++ Temp_Engine/BUILD_LINUX_UBUNTU_RELEASE.sh
@@ -3,5 +3,5 @@
 mkdir -p cmake_bloat
 cd cmake_bloat
 
-konsole --noclose -e cmake ../ -DToLinux=1 -DCMAKE_BUILD_TYPE=Release | tee BuildLog.txt
+konsole --noclose -e cmake ../ -UToAndroid -UToWindows -DToLinux=1 -DCMAKE_BUILD_TYPE=Release | tee BuildLog.txt
 konsole --noclose -e cmake --build . | tee -a BuildLog.txt
